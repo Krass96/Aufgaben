0fe6aeb157b92a566a1c2b0c53b5f98677c1ea4f
Krass96 <gyncoo@abv.bg>
2025-02-18 13:43:55 +0100

    first

---
+++ 3.3.3/aufgabe1.dart
@@ -0,0 +1,14 @@
+void main() {
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  String timeStr = "Zeit";
+  String dd = "!";
+  String c = "Schöne";
+  String morning = "Morgen";
+  String s = "Heute";
+  String spa = " ";
+  int d = 20;
+  int seven = 7;
+  int twelve = 12;
+  int a = 24;
+}
