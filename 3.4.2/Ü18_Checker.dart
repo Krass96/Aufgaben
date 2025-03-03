7e7d940c742f8d859c2e87e08539524e9e9dbcec
Krass96 <gyncoo@abv.bg>
2025-03-03 11:32:32 +0100

    Test 2

--- 3.4.2/Ü18_Checker.dart
+++ 3.4.2/Ü18_Checker.dart
@@ -1,8 +1,10 @@
 void main() {
-  int age = 15;
-  bool hasParentalConstent = true;
+  int age = 13;
+  bool hasParentalConstent = false;
   int movieAgeRating = 16;
 
   if (age >= movieAgeRating || hasParentalConstent && movieAgeRating >= 16)
     print("You can watch the movie");
+  else
+    print("You can't watch the movie");
 }
