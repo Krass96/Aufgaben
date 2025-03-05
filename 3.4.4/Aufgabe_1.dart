af39dadac5cd394de21d268d572c1aecb29361f6
Krass96 <gyncoo@abv.bg>
2025-03-05 11:14:39 +0100

    switch-case

--- 3.4.4/Aufgabe_1.dart
+++ 3.4.4/Aufgabe_1.dart
@@ -3,15 +3,16 @@ enum Weekday { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
 void main() {
   Weekday? weekday;
 
-  if (weekday == Weekday.monday || weekday == Weekday.tuesday) {
-    print('Montage und Dienstage sind anstrengend');
-  } else if (weekday == Weekday.wednesday || weekday == Weekday.thursday) {
-    print('Mittwoch und Donnerstag sind ok');
-  } else if (weekday == Weekday.friday) {
-    print('Freitag ist super');
-  } else if (weekday == Weekday.saturday || weekday == Weekday.sunday) {
-    print('Wochenende ist genial');
-  } else {
-    print('Kein Tag ausgewählt');
+  switch (weekday) {
+    case Weekday.monday || Weekday.tuesday:
+      print('Montage und Dienstage sind angstrengend');
+    case Weekday.wednesday || Weekday.thursday:
+      print('Mittwoch und Donnerstag ist ok');
+    case Weekday.friday:
+      print('Freitag ist super');
+    case Weekday.saturday || Weekday.sunday:
+      print('Wochenende ist genial');
+    default:
+      print('Kein Tag ausgewählt');
   }
 }
