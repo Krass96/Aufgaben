44e6e44ea0e5fddf00832a960b6ffd3e482c7fbe
Krass96 <gyncoo@abv.bg>
2025-03-05 13:04:44 +0100

    meine_lösung

---
+++ 3.4.4/Aufgabe_2.dart
@@ -0,0 +1,51 @@
+void main() {
+  List<String> sex = ['male', 'female'];
+  int age = 20;
+
+  switch (sex) {
+    case ['male']:
+      switch (age) {
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+        case 24:
+          print("Im Schnitt 181,4m");
+        case 25:
+        case 26:
+        case 27:
+        case 28:
+        case 29:
+          print("Im Schnitt 181,3m");
+        case 30:
+        case 31:
+        case 32:
+        case 33:
+        case 34:
+        case 35:
+          print("Im Schnitt 180,4m");
+      }
+    case ['female']:
+      switch (age) {
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+        case 24:
+          print("Im Schnitt 167,5m");
+        case 25:
+        case 26:
+        case 27:
+        case 28:
+        case 29:
+          print("Im Schnitt 167,3 m");
+        case 30:
+        case 31:
+        case 32:
+        case 33:
+        case 34:
+        case 35:
+          print("Im Schnitt 167,2");
+      }
+  }
+}
