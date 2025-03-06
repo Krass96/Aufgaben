0cb6b1aa5992f75f29e19e81e709d8f063ad3d40
Krass96 <gyncoo@abv.bg>
2025-03-06 10:43:40 +0100

    alle Namen ausgeben

---
+++ 3.4.5/Aufgabe_1.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<int> points = [4, 5, 4, 2, 6, 6, 3];
+  List<String> names = [
+    'Julietta',
+    'Benjamino',
+    'Hans-Günther',
+    'Evalinea',
+    'Fiona',
+    'Gregory',
+    'Leonhart',
+  ];
+
+  for (String allNames in names) {
+    print(allNames);
+  }
+}
