cd63c72336fca9c3809ba6f887a7c0f5a17a253f
Krass96 <gyncoo@abv.bg>
2025-03-10 16:10:55 +0100

    corrected

--- 3.5.1/Bonus_1.dart
+++ 3.5.1/Bonus_1.dart
@@ -2,13 +2,13 @@ void main() {
   String word = "abba";
   bool isPalindrom = true;
 
-  for (int i = 0; i < word.length; i++) {
-    if (word[i] != word[i]) {
+  for (int i = 0; i < word.length / 2; i++) {
+    if (word[i] != word[word.length - 1 - i]) {
       isPalindrom = false;
     }
   }
 
-  if (isPalindrom) {
+  if (isPalindrom == true) {
     print('$word ist ein Palindrom');
   } else {
     print('$word ist kein Palindrom');
