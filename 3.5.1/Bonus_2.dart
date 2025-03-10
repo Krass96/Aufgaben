4702067920b436ad8203e198db62c93c564fc4c5
Krass96 <gyncoo@abv.bg>
2025-03-10 15:05:55 +0100

    completed

---
+++ 3.5.1/Bonus_2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int number = 5;
+
+  // Hier deine zwei for-Schleifen:
+
+  for (int i = 1; i <= number; i++) {
+    print(i);
+  }
+  for (int i = number - 1; i > 0; i--) {
+    print(i);
+  }
+}
