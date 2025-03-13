8e8e3932f3d6c9c941d67fe3ae9577d6fd3d829a
Krass96 <gyncoo@abv.bg>
2025-03-13 11:57:01 +0100

    comleted

---
+++ 3.5.3/Aufgabe_2.dart
@@ -0,0 +1,16 @@
+void main() {
+  String t = 'AppAkademie';
+  String e = 'Batch9';
+  String x = 'Krasimir';
+  String s = 'Varnaliev';
+
+  print(sumLetter(t));
+  print(sumLetter(e));
+  print(sumLetter(x));
+  print(sumLetter(s));
+}
+
+int sumLetter(String t) {
+  int sum = t.length;
+  return sum;
+}
