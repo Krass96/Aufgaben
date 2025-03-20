2cf5fd6244e128128924182e3ca4226240d14239
Krass96 <gyncoo@abv.bg>
2025-03-20 14:21:22 +0100

    completed

---
+++ 3.6.2/bonus_3.dart
@@ -0,0 +1,21 @@
+class Rectangle {
+  // Attribute
+  int edgeLength; // x
+  int edgeWidth; // y
+
+  Rectangle(this.edgeLength, this.edgeWidth);
+
+  // Method a)
+  int calculateCircumference() {
+    int x = edgeLength;
+    int y = edgeWidth;
+    return 2 * (y + x);
+  }
+
+  // Method b)
+  int calculateArea() {
+    int x = edgeLength;
+    int y = edgeWidth;
+    return y * x;
+  }
+}
